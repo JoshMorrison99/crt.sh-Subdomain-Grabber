019abbce6c885e9d5466648d3f669ac1cb183e2c
JoshMorrison99 <josh.morrison1999@gmail.com>
2022-11-03 13:57:26 -0400

    Update crt.sh

--- crt.sh
+++ crt.sh
@@ -1,2 +1,3 @@
-curl -s https://crt.sh/?q=$1 > web.txt
-cat web.txt | grep $1 | sed 's/<BR>/\n/g' | sed 's/<\/TD>//' | sed 's/<TD>//' | sed 's/*.//' | tr -d " " | sort | uniq | sed '/<TITLE>/,$!d' | tail -n +2
+curl -s https://crt.sh/?q=$1 > shelled_web.txt
+cat shelled_web.txt | grep $1 | sed 's/<BR>/\n/g' | sed 's/<\/TD>//' | sed 's/<TD>//' | sed 's/*.//' | tr -d " " | sort | uniq | sed '/<TITLE>/,$!d' | tail -n +2
+rm shelled_web.txt
